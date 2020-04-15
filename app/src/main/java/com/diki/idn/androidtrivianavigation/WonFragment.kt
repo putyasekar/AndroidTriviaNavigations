a58a1aea0b31645dbd279a1caa690bb4412f62e7
Putya <putyasekarayu@gmail.com>
2020-04-15 10:54:53 +0700

    commit arguments

--- app/src/main/java/com/diki/idn/androidtrivianavigation/WonFragment.kt
+++ app/src/main/java/com/diki/idn/androidtrivianavigation/WonFragment.kt
@@ -1,10 +1,12 @@
 package com.diki.idn.androidtrivianavigation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.diki.idn.androidtrivianavigation.databinding.FragmentWonBinding
@@ -23,7 +25,28 @@ class WonFragment : Fragment() {
         binding.btnNextMatch.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_wonFragment_to_gameFragment)
         }
+        val args = WonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(
+            context,
+            "NumCorrect: ${args.numCorrect}," +
+                    "NumQuestions: ${args.numQuestions}",
+            Toast.LENGTH_SHORT
+        ).show()
         return binding.root
     }
 
+    private fun getShareIntent(): Intent {
+        val args = WonFragmentArgs.fromBundle((arguments!!))
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text")
+            .putExtra(
+                Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+            )
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
 }
